c9038d49674dd7ec3d209d951622f9bf384904e8
Nas <36938454+Na5co@users.noreply.github.com>
2026-09-17 16:49:08 +0300

    polygo 0.1.0

---
+++ Formula/polygo.rb
@@ -0,0 +1,38 @@
+# Homebrew formula for the tap `Na5co/homebrew-tap` (brew install na5co/tap/polygo).
+# Release automation fills in the version and sha256 values from SHA256SUMS.
+class Polygo < Formula
+  desc "Lokalise for one person: local-first, git-native localization CLI"
+  homepage "https://github.com/Na5co/polygo"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/Na5co/polygo/releases/download/v#{version}/polygo-v#{version}-aarch64-darwin.tar.gz"
+      sha256 "7a36ef2bd12092bb381c01c349e0d43810056e75b0cd873b7230e1e718a88642"
+    end
+    on_intel do
+      url "https://github.com/Na5co/polygo/releases/download/v#{version}/polygo-v#{version}-x86_64-darwin.tar.gz"
+      sha256 "1d05bb8ce92337babda85e116cca622e2774ae8baec702f53acc09dacfabc160"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/Na5co/polygo/releases/download/v#{version}/polygo-v#{version}-aarch64-linux.tar.gz"
+      sha256 "d3c007f9aa2033dafb1a7faff1501f2399e97ac54aee90100e60acb2da9c9eec"
+    end
+    on_intel do
+      url "https://github.com/Na5co/polygo/releases/download/v#{version}/polygo-v#{version}-x86_64-linux.tar.gz"
+      sha256 "a5c6127c91c4812f9094002793b49a6afa7c7ebc6c24c8b257ab7c23ad673078"
+    end
+  end
+
+  def install
+    bin.install "polygo"
+  end
+
+  test do
+    assert_match "polygo", shell_output("#{bin}/polygo --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# homebrew-tap
+
+```sh
+brew install na5co/tap/polygo
+```
+
+Formulae for [polygo](https://github.com/Na5co/polygo). Updated on each release.
